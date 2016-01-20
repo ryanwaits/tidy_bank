7615c41bdd23bd71223b13a493181873392917e5
Anjie Scheuren <emailanjie@gmail.com>
2016-01-20 12:03:16 -0600

    add styling and form

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -6,5 +6,22 @@ class ParentsController < ApplicationController
   end
 
   def new
+    @parent = Parent.new
   end
+
+  def create
+    @parent = Parent.create(parent_params)
+    if @parent.save
+      redirect_to parent_path(@parent)
+    else
+      flash[:notice] = "Please fill out the form!"
+      redirect_to new_parent_path
+    end
+  end
+
+  private
+  def parent_params
+    params.require(:parent).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
+  end
+
 end
