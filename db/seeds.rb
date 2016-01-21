f3c92808ee2a290b945c5d39a3de6cfa7131e67f
Samuel Ryan Waits <ryanwaits@Samuels-MacBook-Air.local>
2016-01-21 12:50:46 -0600

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ Parent.destroy_all
 
 Parent.create([
 
-  { first_name: 'Ryan', last_name: 'Waits', username: 'ryanwaits', email: Faker::Internet.email, password: Faker::Internet.password },
-  { first_name: 'Jay', last_name: 'Bob', username: 'jaybob', email: Faker::Internet.email, password: Faker::Internet.password },
-  { first_name: 'Hank', last_name: 'Williams', username: 'hankwilliams', email: Faker::Internet.email, password: Faker::Internet.password }
+  { first_name: 'Bob', last_name: 'Smith', username: 'bobsmith', email: 'bobsmith@me.com', password: '123456', password_confirmation: '123456' },
+  { first_name: 'John', last_name: 'Walker', username: 'johnwalker', email: 'johnsmith@me.com', password: '123456', password_confirmation: '123456' },
+  { first_name: 'Becky', last_name: 'Jones', username: 'beckyjones', email: 'beckyjones@me.com', password: '123456', password_confirmation: '123456' }
 
   ])
 
@@ -26,5 +26,26 @@ Balance.create([
   { current_balance: 10, balance_due: 0 },
   { current_balance: 5, balance_due: 25 },
   { current_balance: 55, balance_due: 10 }
+
+  ])
+
+Child.create([
+
+  { first_name: 'Ale', last_name: 'Davila', username: 'aledavila', email: 'aledavila@me.com', password: '123456', password_confirmation: '123456', parent_id: Parent.first.id, balance_id: Balance.first.id },
+  { first_name: 'Peggy', last_name: 'Smith', username: 'peggysmith', email: 'peggysmith@me.com', password: '123456', password_confirmation: '123456', parent_id: Parent.first.id, balance_id: Balance.second.id },
+  { first_name: 'Becky', last_name: 'Walker', username: 'beckywalker', email: 'beckywalker@me.com', password: '123456', password_confirmation: '123456', parent_id: Parent.second.id, balance_id: Balance.third.id },
+  { first_name: 'Alex', last_name: 'Jones', username: 'alexjones', email: 'alexjones@me.com', password: '123456', password_confirmation: '123456', parent_id: Parent.third.id, balance_id: Balance.fourth.id },
+  { first_name: 'Bob', last_name: 'Walker', username: 'bobwalker', email: 'bobwalker@me.com', password: '123456', password_confirmation: '123456', parent_id: Parent.second.id, balance_id: Balance.fifth.id }
+
+  ])
+
+Task.create([
+
+  { description: 'Do laundry', completed: false, price: 5.0, due_date: DateTime.strptime("01/25/2016 15:00", "%m/%d/%Y %H:%M"), child_id: Child.first.id },
+  { description: 'Make bed', completed: false, price: 15.0, due_date: DateTime.strptime("01/24/2016 14:00", "%m/%d/%Y %H:%M"), child_id: Child.second.id },
+  { description: 'Do dishes', completed: false, price: 25.0, due_date: DateTime.strptime("01/27/2016 17:00", "%m/%d/%Y %H:%M"), child_id: Child.first.id },
+  { description: 'Vaccum living room', completed: false, price: 15.0, due_date: DateTime.strptime("01/22/2016 18:00", "%m/%d/%Y %H:%M"), child_id: Child.second.id },
+  { description: 'Wash car', completed: false, price: 10.0, due_date: DateTime.strptime("01/25/2016 13:30", "%m/%d/%Y %H:%M"), child_id: Child.third.id },
+  { description: 'Clean windows', completed: false, price: 5.0, due_date: DateTime.strptime("01/25/2016 15:30", "%m/%d/%Y %H:%M"), child_id: Child.fourth.id }
 
   ])
