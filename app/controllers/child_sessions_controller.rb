7902e8d3c48291bee985c15827291d2f4f814dbb
Ryan Waits <ryan.waits@gmail.com>
2016-05-11 22:12:13 -0500

    use respond_with in children controller

--- app/controllers/child_sessions_controller.rb
+++ app/controllers/child_sessions_controller.rb
@@ -1,5 +1,6 @@
 class ChildSessionsController < ApplicationController
   include SessionsHelper
+  respond_to :html
 
   def new
   end
@@ -8,16 +9,22 @@ class ChildSessionsController < ApplicationController
     child = Child.find_by(username: params[:session][:username])
     if child && child.authenticate(params[:session][:password])
       log_in(child)
-      flash[:logged_in] = "Logged in"
-      redirect_to children_path
+      flash.notice = "Logged in"
+      respond_with child, location: children_path
     else
-      flash.error = "Your username or password is incorrect"
-      redirect_to child_login_path
+      set_flash_message
     end
   end
 
   def destroy
     log_out
     redirect_to root_path
+  end
+
+  private 
+
+  def set_flash_message
+    flash.now.notice = "Your username or password is incorrect"
+    redirect_to child_login_path
   end
 end
