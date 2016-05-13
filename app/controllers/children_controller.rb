25fa0ed57921fdcaf083c74a97c0c3ba3670bf05
Ryan Waits <ryan.waits@gmail.com>
2016-05-13 11:56:18 -0500

    set session[:logged_child_id]

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -2,7 +2,7 @@ class ChildrenController < ApplicationController
   before_action :set_child, only: [:show, :edit, :destroy]
   
   def index
-    @tasks = Child.find(session[:child_id]).tasks
+    @tasks = Child.find(session[:logged_child_id]).tasks
   end
 
   def show
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,14 +3,16 @@ module SessionsHelper
       if user.class == Parent
           session[:parent_id] = user.id
           session[:child_id] = nil
+          session[:logged_child_id] = nil
       else
-          session[:child_id] = user.id
+          session[:logged_child_id] = user.id
           session[:parent_id] = nil
+          session[:child_id] = nil
       end
   end
 
   def log_out
-    session[:parent_id], session[:child_id] = nil
+    session[:parent_id], session[:child_id], session[:logged_child_id] = nil
   end
 
 end
